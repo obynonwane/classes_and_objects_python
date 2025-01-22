3c1824633c9e4386c8dcba8d022f3f848424a58c
obynonwane <biostechnologyng@gmail.com>
2025-01-22 14:25:45 +0100

    update

---
+++ robot.py
@@ -0,0 +1,11 @@
+class Robot:
+    def __init__(self, name, color, weight):
+        self.name = name
+        self.color = color
+        self.weight = weight
+    def introduce_self(self):
+        print("My name is" + self.name)
+
+
+r1 = Robot("obinna", "red", 30)
+r1.introduce_self()
